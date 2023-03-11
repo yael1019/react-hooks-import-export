1d6813b3497106804a1e1807f3e6cea96ab42207
Yael Harosh <yaelharosh01@gmail.com>
2023-03-11 14:07:04 -0500

    made some import/export changes

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import aDifferentName from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from './parks/RockyMountain';
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+export default function ColoradoStateParks() {
+  aDifferentName(); // => "42 parks!"
+  console.log(RMFunctions.trees);
+  RMFunctions.wildlife();
+  return(
+    <React.Fragment>
+      <h1>Colorado State Parks!</h1>;
+      <div>
+        <MesaVerde />
+      </div>
+    </React.Fragment>
+  )
 }
